8dedc026d0198211e1840cf285592584f43f0a4c
Alvin Lao <alvin.lao.is@gmail.com>
2014-01-29 11:26:05 -0800

    Move LCD strings

--- Project 1/src/values/constants.asm
+++ Project 1/src/values/constants.asm
@@ -5,7 +5,7 @@
 ;	Define code and global constants here
 ;------------------------------------------------
 
-
+$NOLIST
 CSEG
 ;------------------------------------------------
 ; setup.asm
@@ -33,22 +33,4 @@ DEFAULT1_REFLOWTEMP:	DB 1
 DEFAULT1_REFLOWTIME:	DB 1
 DEFAULT1_COOLRATE:		DB 1
 
-;------------------------------------------------
-; LCD.asm
-;------------------------------------------------
-SETREFLOWRATE_LCD:		DB 'Set Reflow Rate ', '                '
-SETREFLOWTIME_LCD:		DB 'Set Reflow Time ', '                '
-SETREFLOWTEMP_LCD:		DB 'Set Reflow      ', 'Temperature     '
-SETSOAKRATE_LCD:		DB 'Set Soak Rate   ', '                '
-SETCOOLRATE_LCD:		DB 'Set Cool Rate   ', '                '
-SETSOAKTIME_LCD:		DB 'Set Soak Time   ', '                '
-SETSOAKTEMPERATURE_LCD:	DB 'Set Soak        ', 'Temperature     '
-
-PREHEATSOAK_LCD:		DB 'Heating to Soak ', '                '
-SOAK_LCD:				DB 'Soak            ', '                '
-PREHEATREFLOW_LCD:		DB 'Heating to      ', 'Reflow          '
-REFLOW_LCD:				DB 'Reflow          ', '                '
-COOLDOWN_LCD: 			DB 'Cooling down    ', '                '
-STOPPED_LCD:            DB 'Stopped         ', '                '
-FINISHED_LCD: 			DB 'Finished!       ', '                '
-
+$LIST
