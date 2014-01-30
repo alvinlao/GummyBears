b8d7c52aeb836940f159c6bd229ee6452927e6ba
Bibek Kaur <bibekaur@gmail.com>
2014-01-29 17:00:34 -0800

    Update constants.asm

--- Project 1/src/values/constants.asm
+++ Project 1/src/values/constants.asm
@@ -10,20 +10,20 @@ CSEG
 ;------------------------------------------------
 ; setup.asm
 ;------------------------------------------------
-MAXSOAKRATE:			DB 5		;*C/s
-MINSOAKRATE:			DB 1		;*C/s
-MAXSOAKTEMP:			DB 200		;*C
-MINSOAKTEMP:			DB 100		;*C
-MAXSOAKTIME:			DB 120		;seconds
-MINSOAKTIME:			DB 80		;seconds
-MAXREFLOWRATE:			DB 5		;*C/s
-MINREFLOWRATE:			DB 1		;*C/s
-MAXREFLOWTEMP:			DW 300		;*C
-MINREFLOWTEMP:			DB 200		;*C
-MAXREFLOWTIME:			DB 90		;seconds
-MINREFLOWTIME:			DB 30		;seconds
-MAXCOOLRATE:			DB 10		;*C/s
-MINCOOLRATE:			DB 5		;*C/s
+MAX_SOAKRATE:			DB 5		;*C/s
+MIN_SOAKRATE:			DB 1		;*C/s
+MAX_SOAKTEMP:			DB 200		;*C
+MIN_SOAKTEMP:			DB 100		;*C
+MAX_SOAKTIME:			DB 120		;seconds
+MIN_SOAKTIME:			DB 80		;seconds
+MAX_REFLOWRATE:			DB 5		;*C/s
+MIN_REFLOWRATE:			DB 1		;*C/s
+MAX_REFLOWTEMP:			DW 300		;*C
+MIN_REFLOWTEMP:			DB 200		;*C
+MAX_REFLOWTIME:			DB 90		;seconds
+MIN_REFLOWTIME:			DB 30		;seconds
+MAX_COOLRATE:			DB 10		;*C/s
+MIN_COOLRATE:			DB 5		;*C/s
 
 DEFAULT1_SOAKRATE:		DB 3
 DEFAULT1_SOAKTEMP:		DB 150
