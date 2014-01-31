3a76d67d267a4344de5002b33450784525f6cb91
xiazhenjing <xiazhenjing@hotmail.com>
2014-01-31 14:55:34 -0800

    Update buzzer.asm

--- Project 1/src/util/buzzer.asm
+++ Project 1/src/util/buzzer.asm
@@ -6,12 +6,20 @@
 ; NOTE: 
 ;	Requires timer 1
 ;------------------------------------------------
-; Author: 
+; Author: Zhenzheng Xia
 ;------------------------------------------------
 
 $NOLIST
 
 CSEG
+WAIT_HALF_SEC:
+	mov R2, #89
+L3_buzzer: mov R1, #250
+L2_buzzer: mov R0, #250
+L1_buzzer: djnz R0, L1_buzzer
+	djnz R1, L2_buzzer
+	djnz R2, L3_buzzer
+	ret
 	
 ;------------------------------------------------    
 ; + Public function
@@ -20,8 +28,19 @@ CSEG
 ; Setup timer 1 (!) for subsequent buzzer functions
 ;------------------------------------------------
 setup_buzzer:
+   	mov P0MOD, #00000011B ; P0.0, P0.1 are outputs.
+   	setb P0.0
+
+	mov TMOD,  #00010001B ; GATE=0, C/T*=0, M1=0, M0=1: 16-bit timer
+	clr TR1 ; Disable timer 0
+	clr TF1
+	mov TH1, #high(TIMER1_RELOAD)
+	mov TL1, #low(TIMER1_RELOAD)
+	;setb TR1 ; Enable timer 0
+    	setb ET1 ; Enable timer 0 interrupt
+	setb EA  ; Enable all interrupts
 	ret
-	
+
 ;------------------------------------------------    
 ; + Public function
 ;------------------------------------------------
@@ -29,6 +48,9 @@ setup_buzzer:
 ; Makes a short beep
 ;------------------------------------------------
 shortBeep_buzzer:
+	setb tr1
+	lcall WAIT_HALF_SEC
+	clr TR1
 	ret
 	
 ;------------------------------------------------    
@@ -38,6 +60,11 @@ shortBeep_buzzer:
 ; Makes a long beep
 ;------------------------------------------------
 longBeep_buzzer:
+	setb tr1
+	lcall WAIT_HALF_SEC
+	lcall WAIT_HALF_SEC
+	lcall WAIT_HALF_SEC
+	clr TR1
 	ret
 	
 ;------------------------------------------------    
@@ -47,6 +74,13 @@ longBeep_buzzer:
 ; Makes six beeps
 ;------------------------------------------------
 sixBeeps_buzzer:
+	MOV R3, #6
+L0_buzzer:
+	setb tr1
+	lcall WAIT_HALF_SEC
+	clr TR1
+	lcall WAIT_HALF_SEC
+	DJNZ R3, L0_buzzer
 	ret
 
 $LIST
