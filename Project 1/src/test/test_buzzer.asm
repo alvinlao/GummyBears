a420cc413e47e208b99548757298ebcb8fc9471e
David-ubc <david.henderson@alumni.ubc.ca>
2014-02-03 05:22:54 -0400

    buzzer done using interupts

--- Project 1/src/test/test_buzzer.asm
+++ Project 1/src/test/test_buzzer.asm
@@ -8,13 +8,16 @@ $MODDE2
 
 org 0000H
 	ljmp MyProgram
-ORG 001BH
-	ljmp ISR_timer1
+ORG 000BH
+	ljmp ISR_timer0
 DSEG at 30H
 	;TIMERS
 	reload0_timer:			DS 2	; [high] [low]
-	reload1_timer:			DS 2	; [high] [low]
-
+	reload1_timer:			DS 2
+	count1_100_timer:       DS 1
+	buzzer_small:           DS 1	; [high] [low]
+	buzzer_big:				DS 1
+	buzzer_temp:			DS 1
 	count0_100_timer:		DS 1	; Used for 1s calls
 
 	;util/math16.asm	
@@ -33,13 +36,18 @@ $include(../util/buzzer.asm)
 ;------------------------------------------------    
 ; # Protected function
 ;------------------------------------------------
-; ISR_timer1
+; ISR_timer0
 ; Interrupt for buzzer
 ;------------------------------------------------
-ISR_timer1:
-	mov TH1, reload1_timer
-	mov TL1, reload1_timer+1
+ISR_timer0:
 	cpl P1.1
+	mov TH0, reload0_timer
+	mov TL0, reload0_timer+1
+	djnz buzzer_small , skip_buzzer ; decrements the first one 
+	mov buzzer_small , buzzer_temp	; resets the first one
+	djnz buzzer_big , skip_buzzer ; decrements the second onemov buzzer_big , buzzer_tempLoop ; resets the second one
+	lcall stop0_timer
+skip_buzzer:
 	reti
 	
 MyProgram:
@@ -51,7 +59,7 @@ MyProgram:
 
 	;Setup
 	lcall setup_buzzer
-	lcall setup1_timer
+	lcall setup0_timer
 
 	setb EA
 testLoop:
