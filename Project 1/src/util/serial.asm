54ccb0a540e73286ca64f25a098ebcdc52b894a6
Alvin Lao <alvin.lao.is@gmail.com>
2014-02-06 09:55:35 -0800

    Read from serial port setup

--- Project 1/src/util/serial.asm
+++ Project 1/src/util/serial.asm
@@ -15,6 +15,23 @@ CSEG
 ;------------------------------------------------    
 ; + Public function
 ;------------------------------------------------
+; void setup_read_serial( void )
+; For reading (INPUT from serial port)
+; Setup serial ports and timer 2 for baud rate generation
+;------------------------------------------------
+setup_read_serial:
+	clr TR2 ; Disable timer 2
+	mov T2CON, #30H ; RCLK=1, TCLK=1 
+	mov RCAP2H, #high(TIMER2_RELOAD)  
+	mov RCAP2L, #low(TIMER2_RELOAD)
+	setb TR2 ; Enable timer 2
+	mov SCON, #52H
+	setb REN	;Recieve mode
+	ret
+
+;------------------------------------------------    
+; + Public function
+;------------------------------------------------
 ; void setup_write_serial( void )
 ; For writing (OUTPUT from serial port)
 ; Setup serial ports and timer 2 for baud rate generation
