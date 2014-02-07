6cb935623d87a60e9210ce6b8998187facd959f4
Alvin <alvin.lao.is@gmail.com>
2014-02-06 22:32:31 -0800

    Update python

--- Project 1/src/py/Temperature_plot.py
+++ Project 1/src/py/Temperature_plot.py
@@ -6,7 +6,7 @@ import serial
  
 # configure the serial port 
 ser = serial.Serial( 
- port='COM7', 
+ port='COM3', 
  baudrate=115200, 
  parity=serial.PARITY_NONE, 
  stopbits=serial.STOPBITS_TWO, 
